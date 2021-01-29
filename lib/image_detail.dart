a3fd1d9a3184b582238ca88ed4f328a258bdd0f0
olena.mordas <olena.mordas@synpulse.com>
2021-01-29 11:16:38 +0100

    taking picture

---
+++ lib/image_detail.dart
@@ -0,0 +1,48 @@
+// Inside image_detail.dart file
+
+import 'package:flutter/material.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'dart:io';
+import 'dart:ui';
+import 'dart:async';
+
+class DetailScreen extends StatefulWidget {
+  final String imagePath;
+  DetailScreen(this.imagePath);
+
+  @override
+  _DetailScreenState createState() => new _DetailScreenState(imagePath);
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  _DetailScreenState(this.path);
+
+  final String path;
+
+  Size _imageSize;
+  String recognizedText = "Loading ...";
+
+  void _initializeVision() async {
+    // TODO: Initialize the text recognizer here
+  }
+
+  Future<void> _getImageSize(File imageFile) async {
+    // TODO: Retrieve the image size here
+  }
+
+  @override
+  void initState() {
+    _initializeVision();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Details"),
+      ),
+      body: Container(),
+    );
+  }
+}
